da27abfee2fb7d70220d9418b820ddeb465ed18d
Sive Sikutshwa <st10061605@vcconnect.edu.za>
2026-04-16 14:15:44 +0200

    Added EF Core models, DbContext, migration and database setup

--- GLMS/Data/AppDbContext.cs
+++ GLMS/Data/AppDbContext.cs
@@ -1,35 +1,35 @@
 ﻿using GLMS.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.Contracts;
+using ContractEntity = GLMS.Models.Contract;
 
 namespace GLMS.Data
 {
-    // AppDbContext is the main EF Core class used to work with the database
+    // This class manages the connection between the app and the database
     public class AppDbContext : DbContext
     {
-        // Constructor receives database options from Program.cs
+        // Constructor receives the options from Program.cs
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
-        // These DbSet properties represent tables in the database
+        // These properties become tables in the database
         public DbSet<Client> Clients { get; set; }
-        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ContractEntity> Contracts { get; set; }
         public DbSet<ServiceRequest> ServiceRequests { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // One Client can have many Contracts
+            // One client can have many contracts
             modelBuilder.Entity<Client>()
                 .HasMany(c => c.Contracts)
                 .WithOne(c => c.Client)
                 .HasForeignKey(c => c.ClientId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // One Contract can have many Service Requests
-            modelBuilder.Entity<Contract>()
+            // One contract can have many service requests
+            modelBuilder.Entity<ContractEntity>()
                 .HasMany(c => c.ServiceRequests)
                 .WithOne(sr => sr.Contract)
                 .HasForeignKey(sr => sr.ContractId)
